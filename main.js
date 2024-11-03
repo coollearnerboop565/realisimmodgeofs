c1e8a71befbce079bb99c99af6ab5c8444b99f51
coollearnerboop565 <aqukeborn@gmail.com>
2024-11-03 20:47:22 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const githubRepo = "https://github.com/coollearnerboop565/realisimmodgeofs/edit/main/main.json";
 const realisimversion = `0.1`;
-const developstage = true 
+const developstage = true;
+var mod = null; 
+const su35aoa = `95`;
+
 
 (function init() {
+ui.notification.show("This addon is now running")
+    alert("If you would like to cancel this addon please press the restart button on the browser")
+ui.notification.show("Please type a number from 1-5 for the pilot's max G")
+    if(geofs.animation.values.loadFactor >= 3)
+audio.impl.html5.playFile("https://www.youtube.com/watch?v=6yPJH3FZ5C4")
 
-    
-    fetch(`${githubRepo}/styles.css?`+Date.now()).then(async data => {
-        const styleTag = createTag('style',{type:'text/css'});
-        styleTag.innerHTML = await data.text();
-        document.head.appendChild(styleTag);
-    });
+} 
